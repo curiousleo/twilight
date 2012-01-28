096ff4f09d01c4b47b3c8671f0266c72c0bf6fb2
curiousleo <curiousleo@ymail.com>
2012-01-28 19:57:14 +0000

    Compiles physics.cc

--- vector3d.hh
+++ vector3d.hh
@@ -6,7 +6,7 @@ struct Vector3D {
     double x, y, z;
 
     // Constructors
-    Vector3D(Vector3D v) : x(v.x), y(v.y), z(v.z) {}
+    Vector3D() : x(0), y(0), z(0) {}
     Vector3D(double _x, double _y, double _z) :
             x(_x), y(_y), z(_z) {};
     ~Vector3D() {};
@@ -15,29 +15,30 @@ struct Vector3D {
     Vector3D operator-();
 
     // Scalar Multiplication
-    friend Vector3D operator*(int, Vector3D);
     friend Vector3D operator*(double, Vector3D);
-    friend Vector3D operator*(Vector3D, int);
     friend Vector3D operator*(Vector3D, double);
     // Scalar Division
-    friend Vector3D operator/(Vector3D, int);
     friend Vector3D operator/(Vector3D, double);
 
     // Vector3D Arithmetic Operations
     Vector3D operator+(Vector3D);    // vector add
-    Vector3D operator-(Vector3D);    // vector subtract
+    Vector3D operator-(Vector3D) const;    // vector subtract
     double operator*(Vector3D);    // inner dot product
 
     Vector3D& operator*=(double);      // vector scalar mult
     Vector3D& operator/=(double);      // vector scalar div
 
     // Comparison
-    int operator==(Point);
-    int operator!=(Point);
+    int operator==(Vector3D);
+    int operator!=(Vector3D);
+
+    // Shorthand Operations
+    Vector3D& operator+=(Vector3D);      // vector increment
+    Vector3D& operator-=(Vector3D);      // vector decrement
     
-    // Point Relations
-    friend double d(Point, Point);         // Distance
-    friend double d2(Point, Point);        // Distance^2
+    // Vector3D Relations
+    friend double d(Vector3D, Vector3D);         // Distance
+    friend double d2(Vector3D, Vector3D);        // Distance^2
 
     // Vector3D Properties
     double len() {
@@ -48,11 +49,11 @@ struct Vector3D {
     }
 
     // Input/Output streams
-    friend istream& operator>>(istream&, Point&);
-    friend ostream& operator<<(ostream&, Point);
+    friend std::istream& operator>>(std::istream&, Vector3D&);
+    friend std::ostream& operator<<(std::ostream&, Vector3D);
 
     // Special Operations
     void normalize();
 };
 
-#endif GUARD_vector_hh
+#endif

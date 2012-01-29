3161e127d3f858f031ff1fcea139bf780ffd4208
curiousleo <curiousleo@ymail.com>
2012-01-29 15:24:10 +0000

    Show eclipses

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ using namespace std;
 
 int main(void) {
     unsigned int i;
-    System s;
+    double dt = 0.001;	// days
+    int lasteclipse = -1;
+    System s(dt);
 
     cin >> i;
-
     for (unsigned int j = 0; j != i; j++) {
         cin >> s;
     }
 
-    cout << s << endl;
+    for (double t = 0; t < 10000; t += dt) {
+        if (s.pulse() && floor(t) != lasteclipse) {
+	    lasteclipse = floor(t);
+	    cerr << t+dt << " ";
+	}
+        // cout << s;
+    }
+    cerr << endl;
     return 0;
 }

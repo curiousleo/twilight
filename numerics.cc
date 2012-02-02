7c93a3451452a56c1440f287cb43ca996f401a08
curiousleo <curiousleo@ymail.com>
2012-02-02 20:13:30 +0000

    Arguments explained

--- numerics.cc
+++ numerics.cc
@@ -3,9 +3,15 @@
 using namespace std;
 
 void euler(Vector3D& rs, Vector3D& vs, function<vector<Vector3D>()> a) {
+    // rs and vs are the initial positions and velocities, respectively
+    // a is a function that takes the list of positions and returns a
+    // list of accelerations
     return;
 }
 
 void rk4(Vector3D& rs, Vector3D& vs, function<vector<Vector3D>()> a) {
+    // rs and vs are the initial positions and velocities, respectively
+    // a is a function that takes the list of positions and returns a
+    // list of accelerations
     return;
 }

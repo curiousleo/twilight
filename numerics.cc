4541f65ae7a37423934ee6a89aa70d209c3c8824
curiousleo <curiousleo@ymail.com>
2012-02-03 20:21:20 +0000

    Removed RK4 explanation

--- numerics.cc
+++ numerics.cc
@@ -37,27 +37,6 @@ void rk4(System* system) {
     // rs and vs are the initial positions and velocities, respectively
     // a is a function that takes the list of positions and returns a
     // list of accelerations
-
-    /*
-    x1 = x
-    v1 = v
-    a1 = a(x1, v1, 0)
-
-    x2 = x + 0.5*v1*dt
-    v2 = v + 0.5*a1*dt
-    a2 = a(x2, v2, dt/2.0)
-
-    x3 = x + 0.5*v2*dt
-    v3 = v + 0.5*a2*dt
-    a3 = a(x3, v3, dt/2.0)
-
-    x4 = x + v3*dt
-    v4 = v + a3*dt
-    a4 = a(x4, v4, dt)
-
-    xf = x + (dt/6.0)*(v1 + 2*v2 + 2*v3 + v4)
-    vf = v + (dt/6.0)*(a1 + 2*a2 + 2*a3 + a4)
-    */
     Array3Xd r1 = system->rs;
     Array3Xd v1 = system->vs;
     Array3Xd a1 = system->gravitate(r1);

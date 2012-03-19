245ba868a95d6addbcedf38571c81c75fbb32bbf
curiousleo <curiousleo@ymail.com>
2012-03-19 10:15:06 +0000

    Added header comment

--- misc/list_eclipses.py
+++ misc/list_eclipses.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+
+# Twilight
+#
+# list_eclipses.py
+# Download, parse and print Wikipedia's list of 21st century solar
+# eclipses.
+
 '''
 List all solar eclipses in the 21st century (from Wikipedia).
 Requires Python 3 and lxml (lxml.de).

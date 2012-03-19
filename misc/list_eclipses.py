8c65d6e87ab2cea3c28d9539a84953807a3a25ba
curiousleo <curiousleo@ymail.com>
2012-03-19 10:10:43 +0000

    list_eclipses.py: Rewritten in Python 3 using lxml: MUCH faster

--- misc/list_eclipses.py
+++ misc/list_eclipses.py
@@ -1,46 +1,37 @@
-#!/usr/bin/python -O
-
 '''
 List all solar eclipses in the 21st century (from Wikipedia).
-Requires Python 2.7 and BeautifulSoup 4.
+Requires Python 3 and lxml (lxml.de).
 
-Usage: python load_eclipses.py > eclipses.txt
+Usage: python list_eclipses.py > eclipses.txt
 '''
 
-# Import 'print' function so we can use it as an argument to 'map'
-from __future__ import print_function
-
-from bs4 import BeautifulSoup as beautiful  # HTML parser
-from urllib2 import build_opener            # Download web page
-import re                                   # Regular expressions
+from lxml import html, etree                # HTML & XPath
+from urllib import request                  # Download web page
 
 URL = 'https://en.wikipedia.org/wiki/' +\
     'List_of_solar_eclipses_in_the_21st_century'
-RE = re.compile(r'^\d{4}-\d{2}-\d{2}$')     # Format 2008-08-01
+XPATH = r'//span[@class="sortkey" and ' +\
+    r're:test(., "^\d{4}-\d{2}-\d{2}$")]/text()'
+RE_NS = 'http://exslt.org/regular-expressions'
 
 def load_html (url):
     '''
-    Download url and return BeautifulSoup-parsed HTML
+    Download url and return lxml-parsed HTML
     '''
-    opener = build_opener()
     # Pretend to be Mozilla
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    req = request.Request(
+        url=url, headers={'User-agent' : 'Mozilla/5.0'})
     # Download and parse
-    return beautiful(opener.open(url))
-
-def date_filter (tag):
-    '''
-    Tag filter function
-    '''
-    return tag.get('class') == [u'sortkey'] \
-      and RE.match(tag.string.strip())
+    return html.parse(request.urlopen(req)).getroot()
 
 def print_eclipses ():
     '''
     Print a list of all solar eclipses in the 21st century.
     '''
-    eclipses = load_html(URL).find_all(date_filter)
-    map(print, [tag.string.strip() for tag in eclipses])
+    # Compile XPath expression
+    xpath = etree.XPath(XPATH, namespaces={'re' : RE_NS})
+    # Print all matches
+    for el in xpath(load_html(URL)): print(el)
 
 # Running as a program?
 if __name__ == '__main__':
